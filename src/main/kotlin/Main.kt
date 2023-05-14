aa07c7af8fb89d54efc8402e550d2594f10abf2d
fbn776 <fbn776@gmail.com>
2023-05-14 23:41:01 +0530

    Updated README.md

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,14 +5,17 @@ fun <T>MutableList<T>.lastElm(): T {
 
 fun main() {
 
-    val ls: Queue<Char> = Queue<Char>(*("1*2/2".toCharArray().toTypedArray()))
+    val ls: Queue<Char> = Queue<Char>(*("1*2/-2".toCharArray().toTypedArray()))
 
     println("The queue is: $ls")
 
     val infixNot = InfixNotation().convert(ls)
+
+    println(infixNot.result)
     val result = infixNot.result
     var solver = SolveInfix().solve(result)
 
+    println("Has error: ${infixNot.hasDecimalError || infixNot.hasOperatorError || infixNot.hasInvalidCharError}")
     println("The infix notation is: $result")
     println("The solved is: ${solver}")
 }
